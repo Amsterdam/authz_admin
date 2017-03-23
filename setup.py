0d74b6f503d0261bafc026d56515cca31c882516
Pieter van Beek <pieter@djinnit.com>
2017-03-23 21:49:15 +0100

    Initial project structure and plumbing

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+setup(
+    version='0.1',
+    name='datapunt-oauth2',
+    description="Permission Management and OAuth2 Authorization Service",
+    #long_description="",
+    url='https://github.com/DatapuntAmsterdam/oauth2',
+    author='Amsterdam Datapunt',
+    author_email='datapunt.ois@amsterdam.nl',
+    license='Mozilla Public License Version 2.0',
+    classifiers=[
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ],
+    #cmdclass={'test': PyTest},
+    packages=['admin', 'common', 'oauth2'],
+    install_requires=[],
+    extras_require={
+        'doc': ['sphinx', 'sphinx_rtd_theme']
+    },
+    tests_require=[],
+)
