4266aef48d650199fd947f1873063f9aa71c35e1
Pieter van Beek <pieter@djinnit.com>
2017-10-19 17:17:17 +0200

    fix2

--- src/authz_admin/handlers/__init__.py
+++ src/authz_admin/handlers/__init__.py
@@ -1,5 +1,5 @@
 from ._accounts import Account, Accounts
-from ._authorization import authorization
+#from ._authorization import authorization
 from ._profiles import Profiles, Profile
 from ._roles import Role, Roles
 from ._root import Root
