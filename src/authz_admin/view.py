dea1126d3b5b992a1a651b6391d9ed64d15afb37
Pieter van Beek <pieter@djinnit.com>
2017-12-08 22:03:14 +0100

    fix for security problem pen and hack

--- src/authz_admin/view.py
+++ src/authz_admin/view.py
@@ -67,22 +67,4 @@ class OAuth2View(rest_utils.View, metaclass=abc.ABCMeta):
 
     @authorization.authorize('GET')
     async def to_dict(self):
-        swagger = self.request.app['swagger']
-        base_path = swagger.base_path
-        paths = swagger.specification['paths']
-        path, _ = swagger.get_path_spec(
-            self.rel_url.raw_path
-        )
-        assert path is not None
-        assert path.startswith(base_path)
-        path = path[len(base_path):]
-        if 'get' not in paths[path]:
-            raise web.HTTPMethodNotAllowed(
-                'GET', list([
-                    method.upper() for method in paths[path].keys()
-                ])
-            )
-        method_info = paths[path]['get']
-        if 'security' in method_info:
-            await authorization.enforce_one_of(self.request, method_info['security'])
         return await super().to_dict()
